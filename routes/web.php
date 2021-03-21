a9f09bf23bc5c5b821422a1ca174efb7bc35dc90
lecbit <artm-marych@rambler.ru>
2021-03-21 15:55:25 +0100

    template+webpack

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\MainController;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,4 +16,9 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+
+Route::group(['prefix' => 'admin', 'namespace'=>'App\Http\Controllers\Admin'],function(){
+    Route::get('/', 'MainController@index')->name('admin.index');
 });
